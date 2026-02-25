53a0d0abd7da425ab6ed8d25e3d82c60956918dd
Cake <therealcakeoftruth@gmail.com>
2026-02-24 20:39:11 -0500

    Updated ESP32 sample program

--- ESP32/esp32/esp32.ino
+++ ESP32/esp32/esp32.ino
@@ -1,21 +1,26 @@
 #include <ArduinoBLE.h>
 
+// Randomly generated UUIDs for the service & characteristic
 char* serviceUUID = "3104838b-5ed7-4e6c-ac03-7823dd9d4c7b";
 char* characteristicUUID = "77283f94-81cf-4438-be8a-642fe725ccbd";
 
+// Set up the service & characteristic
+BLEService myService(serviceUUID);
+BLEByteCharacteristic myChar(characteristicUUID,  BLERead | BLENotify);
+
 void setup() {
-  // put your setup code here, to run once:
+  // Serial (for testing)
   Serial.begin(9600);
 
-  BLEService myService(serviceUUID);
-  BLEByteCharacteristic myChar(characteristicUUID,  BLERead | BLENotify);
-
+  // Set up the bluetooth device & service
   BLE.begin();
   BLE.setLocalName("MyArduinoDevice");
   BLE.setAdvertisedService(myService);
   myService.addCharacteristic(myChar);
+  myChar.writeValue(0);
   BLE.addService(myService);
 
+  // Advertise bluetooth
   BLE.advertise();
 }
 
@@ -29,8 +34,11 @@ void loop() {
     Serial.println(central.address());
     
     // While the central is connected
+    // Dummy code for proof of concept
+    int t = 0;
     while (central.connected()) {
-      
+      myChar.writeValue(++t);
+      delay(200);
     }
 
     // When the central disconnects
